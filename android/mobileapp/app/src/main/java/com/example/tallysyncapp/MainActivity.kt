76345fb74c7f54306d891cfa95338cf359c8e3ab
ziaur <ziaur.rahman@webmail.com>
2026-07-28 14:15:20 +0200

    basic app update

--- android/mobileapp/app/src/main/java/com/example/tallysyncapp/MainActivity.kt
+++ android/mobileapp/app/src/main/java/com/example/tallysyncapp/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.fillMaxSize
+import androidx.compose.ui.Modifier
 import com.example.tallymobile.ui.TallyMobileApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MaterialTheme {
-                Surface {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
                     TallyMobileApp()
                 }
             }
